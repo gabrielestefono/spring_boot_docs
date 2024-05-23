352293bc6a558a558beefbb54cd3846077e57720
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-23 08:53:46 -0300

    Criando arquivos DOCS

--- spring_security/demo/src/main/java/com/demo/config/SecurityConfig.java
+++ spring_security/demo/src/main/java/com/demo/config/SecurityConfig.java
@@ -11,27 +11,33 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+		http
+			.authorizeHttpRequests(authorize -> authorize
+				.anyRequest().authenticated()
+			)
 			.httpBasic(Customizer.withDefaults())
 			.formLogin(Customizer.withDefaults());
 
-			return http.build();
+		return http.build();
 	}
 
 	@Bean
-	public UserDetailsService userDetailsService(){
+	public UserDetailsService userDetailsService() {
+		@SuppressWarnings("deprecation")
 		UserDetails userDetails = User.withDefaultPasswordEncoder()
 			.username("user")
 			.password("password")
-			.roles("User")
+			.roles("USER")
 			.build();
 
-			return new InMemoryUserDetailsManager(userDetails);
+		return new InMemoryUserDetailsManager(userDetails);
 	}
+
 }
